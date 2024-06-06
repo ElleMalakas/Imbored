62b05f841cb13b01beb3acbf781fb315079899b8
ElleMalakas <ellegrowtopia18@gmail.com>
2024-06-07 00:48:31 +0800

    im sleepppppppy

---
+++ script.js
@@ -0,0 +1,21 @@
+//this is what we call a variable, which is used to store data values
+let exampleMessage = "this is a message"; 
+let number = 18; //number
+let names = "Elle"; //string
+let isStudent = true; //boolean
+console.log("hi, this prints whatever you write inside this parenthesis into the terminal/cmd")
+console.log(exampleMessage + number + names + isStudent); //you can connect all the variables together!
+let sum = 5 + 3; //this basically prints what the answer of this math question
+let product = 5 * 3;
+console.log(sum, product)
+//exercise 1
+let a = 10
+let b = 10
+let c = 20
+console.log(a == b)
+console.log(a === b)
+console.log(a != b)
+console.log(a !== b)
+console.log(c > a)
+console.log(c < b)
+//exercise 2
