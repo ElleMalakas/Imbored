ccfc4bafd945225ae6bb042bec7c27879b9b05c4
ElleMalakas <ellegrowtopia18@gmail.com>
2024-06-07 12:17:24 +0800

    variables dones

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log(' ───♡─── All About Variables! ───♡─── ')
 //this is what we call a variable, which is used to store data values
 let exampleMessage = "this is a message"; 
 let number = 18; //number
@@ -18,4 +19,17 @@ console.log(a != b)
 console.log(a !== b)
 console.log(c > a)
 console.log(c < b)
-//exercise 2
+//exercise 1.2
+let name1 = 'olly'
+let age1 = 20
+let isStudent1 = true
+console.log(name1)
+console.log(age1)
+console.log(isStudent1)
+//exercise 1.3
+let num1 = 10
+let num2 = 5
+console.log(num1 + num2); //or we could also make variables for each one of the arithmetic operators
+console.log(num1 - num2); //this is fine because i dont want the code to be too long.
+console.log(num1 * num2)
+console.log(num1 / num2)
