a6f1341d709fb05ecf300185fa8218e82a6667d2
ElleMalakas <ellegrowtopia18@gmail.com>
2024-06-07 15:46:55 +0800

    finish functions

--- script_functions.js
+++ script_functions.js
@@ -19,3 +19,23 @@ function add(a, b) {
 let sum1 = add(5, 3)
 console.log(sum1)
 console.log(add(5, 3)); //you can also do this
+//exercise 4
+function sayHello() {
+    console.log('Hello, Javascript!') //this wouldn't print in the terminal/cmd unless you call the function to perform it.
+}
+sayHello(); //this makes it print now
+//exercise 4.2
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(4, 5))
+//exercise 4.3
+const subtract = (a, b) => {
+    return a - b
+}
+console.log(subtract(10, 3))
+//exercise 4.4
+const divide = function(a, b) {
+    return a / b;
+}
+console.log(divide(20, 4))
