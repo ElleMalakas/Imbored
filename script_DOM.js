62ea957fbea2d58d49641652c1fcc64f8a61fd75
ElleMalakas <ellegrowtopia18@gmail.com>
2024-06-07 17:27:34 +0800

    rese

---
+++ script_DOM.js
@@ -0,0 +1,11 @@
+console.log(' ───♡─── All About DOM ───♡─── '); //javascript can manipulate the website or HTML document
+    // let element = document.getElementById('myId') //select one element
+    // console.log(element)
+    // let elements = document.getElementsByClassName('myClass') //selects all elements inside the class name
+    // console.log(elements)
+    // let elements2 = document.getElementsByTagName('div') //select all elements inside the tag 
+    // console.log(elements2)
+    // let elements3 = document.querySelector('.myClass') //selects the element with a CSS selector
+    // console.log(elements3)
+    // let elements4 = document.querySelectorAll('.myClass')
+    // console.log(elements4)
