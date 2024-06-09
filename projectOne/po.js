9d6b29455007c60c7dd71e2480d01c981d723205
ElleMalakas <ellegrowtopia18@gmail.com>
2024-06-10 00:24:16 +0800

    done simple to do list.. nice to just upgrade or style it

--- projectOne/po.js
+++ projectOne/po.js
@@ -5,24 +5,33 @@ addButton.addEventListener('click', function () {
 
     if (whatTask) { //lesson learned, always in-order
         let li = document.createElement('li')
-        li.innerHTML = `${whatTask} `
+        li.innerHTML = whatTask
+        li.style.margin = '5px'
 
         let btn = document.createElement('button')
         btn.innerHTML = 'Remove'
+        btn.style.marginLeft = '5px'
         btn.addEventListener('click', function() {
             li.remove()
+            console.log(`(⛔) ${whatTask} : task has been removed ! `)
         })
 
         let btn2 = document.createElement('button')
         btn2.innerHTML = 'Done'
+        btn.style.marginRight = '5px'
         btn2.addEventListener('click', function() {
-            li.style.textDecoration = 'linethrough'
+            if (li.style.textDecoration === 'line-through') {
+                 li.style.textDecoration = 'none'
+                 console.log(`(❌) ${whatTask} : task has been unchecked !`)
+            } else {
+                 li.style.textDecoration = 'line-through'
+                 console.log(`(✅) ${whatTask} : task has been finished !`)
+            }
+            
         })
-
         li.appendChild(btn); //lesson learned, only append once at a time
         li.appendChild(btn2)
         document.body.appendChild(li)
-
         console.log('add button has been clicked and added a task ! 💢')
     }
 })
