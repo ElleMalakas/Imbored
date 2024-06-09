a7004eb47308a43eb9dbd5fa68cf1e909a8d0341
ElleMalakas <ellegrowtopia18@gmail.com>
2024-06-10 00:08:01 +0800

    tajfpoieafe

--- projectOne/po.js
+++ projectOne/po.js
@@ -1,10 +1,28 @@
 let addButton = document.getElementById('add');
 
-addButton.addEventListener('click', function() {
+addButton.addEventListener('click', function () {
     let whatTask = prompt('What task would you like to add ?')
-    let li = document.createElement('li')
-    if (whatTask) {
-        document.body.appendChild(li).innerHTML = whatTask
-        console.log('add button has been clicked and added a task ! 💢') 
+
+    if (whatTask) { //lesson learned, always in-order
+        let li = document.createElement('li')
+        li.innerHTML = `${whatTask} `
+
+        let btn = document.createElement('button')
+        btn.innerHTML = 'Remove'
+        btn.addEventListener('click', function() {
+            li.remove()
+        })
+
+        let btn2 = document.createElement('button')
+        btn2.innerHTML = 'Done'
+        btn2.addEventListener('click', function() {
+            li.style.textDecoration = 'linethrough'
+        })
+
+        li.appendChild(btn); //lesson learned, only append once at a time
+        li.appendChild(btn2)
+        document.body.appendChild(li)
+
+        console.log('add button has been clicked and added a task ! 💢')
     }
 })
