676b27f0320ad8d044cae7150fb2b19a045e6cf5
ElleMalakas <ellegrowtopia18@gmail.com>
2024-06-09 23:41:04 +0800

    ughh

--- projectOne/po.js
+++ projectOne/po.js
@@ -2,10 +2,9 @@ let addButton = document.getElementById('add');
 
 addButton.addEventListener('click', function() {
     let whatTask = prompt('What task would you like to add ?')
-    let createElement = document.createElement('li')
+    let li = document.createElement('li')
     if (whatTask) {
-        document.body.appendChild(createElement).innerHTML = whatTask
+        document.body.appendChild(li).innerHTML = whatTask
         console.log('add button has been clicked and added a task ! 💢') 
     }
-
 })
