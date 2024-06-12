413bfb48ba7548f320a8f11c08db30ef9dafe0e8
ElleMalakas <ellegrowtopia18@gmail.com>
2024-06-12 22:12:45 +0800

    finally

--- projectOne/po.js
+++ projectOne/po.js
@@ -1,9 +1,8 @@
-let addedTask = false
 let addButton = document.getElementById('add');
-
+let currentTheme = 'styleA';
 addButton.addEventListener('click', function () {
     let whatTask = prompt('What task would you like to add ?')
-    addedTask = true
+
     if (whatTask) { //lesson learned, always in-order
         let box = document.createElement('div')
         box.style.display = 'flex'
@@ -28,8 +27,8 @@ addButton.addEventListener('click', function () {
         let btn = document.createElement('button')
         let img = document.createElement('img')
         btn.appendChild(img)
-        img.id = 'img1'
-        img.src = 'public/remove.png'
+        img.className = 'img1'
+        img.src = getImgSrc('img1', currentTheme)
         btn.id = "removeBtn"
         btn.addEventListener('click', function () {
             li.remove()
@@ -44,8 +43,8 @@ addButton.addEventListener('click', function () {
         let btn2 = document.createElement('button')
         let img2 = document.createElement('img')
         btn2.appendChild(img2)
-        img2.id = 'img2'
-        img2.src = 'public/check.png'
+        img2.className = 'img2'
+        img2.src = getImgSrc('img2', currentTheme)
         btn2.id = 'doneBtn'
         btn2.addEventListener('click', function () {
             if (li.style.textDecoration === 'line-through') {
@@ -61,8 +60,8 @@ addButton.addEventListener('click', function () {
         let btn3 = document.createElement('button')
         let img3 = document.createElement('img')
         btn3.appendChild(img3)
-        img3.id = 'img3'
-        img3.src = 'public/edit.png'
+        img3.className = 'img3'
+        img3.src = getImgSrc('img3', currentTheme)
         btn3.id = 'editBtn'
         btn3.addEventListener('click', function () {
             let editedTask = prompt('Editing Task ...')
@@ -85,77 +84,108 @@ addButton.addEventListener('click', function () {
         taskDesc.appendChild(li)
 
         document.body.appendChild(box)
-
-
-        // li.appendChild(btn); //lesson learned, only append once at a time
-        // li.appendChild(btn2)
-        // li.appendChild(btn3)
-        // document.body.appendChild(li)
-
         console.log('add button has been clicked and added a task ! 💢')
     }
 })
-
-function updateImg(theme) {
-    if (addedTask) {
-        switch (theme) {
-            case 'A':
-                document.getElementById('img1').src = 'public/remove.png'
-                document.getElementById('img2').src = 'public/check.png'
-                document.getElementById('img3').src = 'public/edit.png'
-                break;
-            case 'B':
-                document.getElementById('img1').src = 'public/trashB.png'
-                document.getElementById('img2').src = 'public/checkB.png'
-                document.getElementById('img3').src = 'public/editB.png'
-                break;
-            case 'C':
-                document.getElementById('img1').src = 'public/trashC.png'
-                document.getElementById('img2').src = 'public/checkC.png'
-                document.getElementById('img3').src = 'public/editC.png'
-                break;
-            case 'D':
-                document.getElementById('img1').src = 'public/trashD.png'
-                document.getElementById('img2').src = 'public/checkD.png'
-                document.getElementById('img3').src = 'public/editD.png'
-                break;
-            default:
-                break;
-        }
-    }
+function applyTheme(theme, addImgSrc, img1Src, img2Src, img3Src) {
+    let newStyle = document.createElement('link');
+    newStyle.rel = 'stylesheet';
+    newStyle.href = theme + '.css';
+    document.head.appendChild(newStyle);
+    document.getElementById('addimg').src = addImgSrc;
+    document.querySelectorAll('.img1').forEach(img => img.src = img1Src);
+    document.querySelectorAll('.img2').forEach(img => img.src = img2Src);
+    document.querySelectorAll('.img3').forEach(img => img.src = img3Src);
 }
 
 function themeChange() {
     document.body.addEventListener('click', function (event) {
         let targetId = event.target.id;
         if (targetId === 'abtn') {
-            let newstyleA = document.createElement('link')
-            newstyleA.rel = 'stylesheet'
-            newstyleA.href = 'styleA.css'
-            document.head.appendChild(newstyleA)
-            document.getElementById('addimg').src = 'public/add.png'
-            updateImg('A')
+            applyTheme('styleA', 'public/add.png', 'public/remove.png', 'public/check.png', 'public/edit.png'); //theme, addImgSrc, img1Srec, img2Src, img3Src
+            currentTheme = 'styleA';
         } else if (targetId === 'bbtn') {
-            let newstyleB = document.createElement('link')
-            newstyleB.rel = 'stylesheet'
-            newstyleB.href = 'styleB.css'
-            document.head.appendChild(newstyleB)
-            document.getElementById('addimg').src = 'public/addB.png'
-            updateImg('B')
+            applyTheme('styleB', 'public/addB.png', 'public/trashB.png', 'public/checkB.png', 'public/editB.png');
+            currentTheme = 'styleB';
         } else if (targetId === 'cbtn') {
-            let newstyleC = document.createElement('link')
-            newstyleC.rel = 'stylesheet'
-            newstyleC.href = 'styleC.css'
-            document.head.appendChild(newstyleC)
-            document.getElementById('addimg').src = 'public/addC.png'
-            updateImg('C')
+            applyTheme('styleC', 'public/addC.png', 'public/trashC.png', 'public/checkC.png', 'public/editC.png');
+            currentTheme = 'styleC';
         } else if (targetId === 'dbtn') {
-            let newstyleD = document.createElement('link')
-            newstyleD.rel = 'stylesheet'
-            newstyleD.href = 'styleD.css'
-            document.head.appendChild(newstyleD)
-            document.getElementById('addimg').src = 'public/addD.png'
-            updateImg('D')
+            applyTheme('styleD', 'public/addD.png', 'public/trashD.png', 'public/checkD.png', 'public/editD.png');
+            currentTheme = 'styleD';
         }
     })
 }
+
+function getImgSrc(imgType, theme) {
+    switch (theme) {
+        case 'styleA':
+            if (imgType === 'img1') return 'public/remove.png';
+            if (imgType === 'img2') return 'public/check.png';
+            if (imgType === 'img3') return 'public/edit.png';
+            break;
+        case 'styleB':
+            if (imgType === 'img1') return 'public/trashB.png';
+            if (imgType === 'img2') return 'public/checkB.png';
+            if (imgType === 'img3') return 'public/editB.png';
+            break;
+        case 'styleC':
+            if (imgType === 'img1') return 'public/trashC.png';
+            if (imgType === 'img2') return 'public/checkC.png';
+            if (imgType === 'img3') return 'public/editC.png';
+            break;
+        case 'styleD':
+            if (imgType === 'img1') return 'public/trashD.png';
+            if (imgType === 'img2') return 'public/checkD.png';
+            if (imgType === 'img3') return 'public/editD.png';
+            break;
+    }
+}
+
+themeChange()
+
+
+
+// function themeChange() {
+//     document.body.addEventListener('click', function (event) {
+//         let targetId = event.target.id;
+//         if (targetId === 'abtn') {
+//             let newstyleA = document.createElement('link')
+//             newstyleA.rel = 'stylesheet'
+//             newstyleA.href = 'styleA.css'
+//             document.head.appendChild(newstyleA)
+//             document.getElementById('addimg').src = 'public/add.png'
+//             document.querySelectorAll('.img1').forEach(img => img.src = 'public/remove.png')
+//             document.querySelectorAll('.img2').forEach(img => img.src = 'public/check.png')
+//             document.querySelectorAll('.img3').forEach(img => img.src = 'public/edit.png')
+//         } else if (targetId === 'bbtn') {
+//             let newstyleB = document.createElement('link')
+//             newstyleB.rel = 'stylesheet'
+//             newstyleB.href = 'styleB.css'
+//             document.head.appendChild(newstyleB)
+//             document.getElementById('addimg').src = 'public/addB.png'
+//             document.querySelectorAll('.img1').forEach(img => img.src = 'public/trashB.png')
+//             document.querySelectorAll('.img2').forEach(img => img.src = 'public/checkB.png')
+//             document.querySelectorAll('.img3').forEach(img => img.src = 'public/editB.png')
+//         } else if (targetId === 'cbtn') {
+//             let newstyleC = document.createElement('link')
+//             newstyleC.rel = 'stylesheet'
+//             newstyleC.href = 'styleC.css'
+//             document.head.appendChild(newstyleC)
+//             document.getElementById('addimg').src = 'public/addC.png'
+//             document.querySelectorAll('.img1').forEach(img => img.src = 'public/trashC.png')
+//             document.querySelectorAll('.img2').forEach(img => img.src = 'public/checkC.png')
+//             document.querySelectorAll('.img3').forEach(img => img.src = 'public/editC.png')
+//         } else if (targetId === 'dbtn') {
+//             let newstyleD = document.createElement('link')
+//             newstyleD.rel = 'stylesheet'
+//             newstyleD.href = 'styleD.css'
+//             document.head.appendChild(newstyleD)
+//             document.getElementById('addimg').src = 'public/addD.png'
+//             document.querySelectorAll('.img1').forEach(img => img.src = 'public/trashD.png')
+//             document.querySelectorAll('.img2').forEach(img => img.src = 'public/checkD.png')
+//             document.querySelectorAll('.img3').forEach(img => img.src = 'public/editD.png')
+//         }
+//     })
+// } themeChange(); //this completely work but I have to change it due to problems with not updating images during adding a task.
+
